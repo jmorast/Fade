a3b991ed41832f6d8fcc2938a58757096ef6dc1d
jmorast <jmorast@gmail.com>
2013-04-27 00:16:39 -0400

    working mouse

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -1,6 +1,8 @@
 package;
 
+import nme.ui.Mouse;
 import nme.Assets;
+import nme.display.Sprite;
 import nme.geom.Rectangle;
 import nme.net.SharedObject;
 import org.flixel.FlxButton;
@@ -11,27 +13,32 @@ import org.flixel.FlxSprite;
 import org.flixel.FlxState;
 import org.flixel.FlxText;
 import org.flixel.FlxU;
-import org.flixel.FlxPoint;
-//import org.flixel.FlxMouse;
+import org.flixel.system.input.Mouse;
+
 
 class MenuState extends FlxState
 {
 	
 	var Player:FlxSprite;
-	var Mouse:FlxMouse;
 	
 	override public function create():Void
 	{
 		
 		FlxG.bgColor = 0xffffffff;	
-		FlxG.mouse.hide();
+		
 		
 		Player = new FlxSprite(320,249);
 		Player.makeGraphic(20,20);
 		Player.color=0x000000;
 		add(Player);
 		
-		Mouse = new FlxMouse();
+		var mpoint:FlxSprite = new FlxSprite(0,0);
+		mpoint.makeGraphic(1,1);
+		mpoint.alpha=0;
+		
+		FlxG.mouse.show(mpoint,0.01);
+		
+		
 		
 		
 	}
@@ -44,6 +51,9 @@ class MenuState extends FlxState
 	override public function update():Void
 	{
 		super.update();
+		
+		Player.x=FlxG.mouse.screenX;
+		Player.y=FlxG.mouse.screenY;
 		
 		
 	}	
